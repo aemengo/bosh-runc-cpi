8f8f685df21cd9c6f4f88b3f99bca406e3b9dee8
Anthony Emengo <anthonyemengojr@gmail.com>
2018-08-23 01:19:03 -0400

    Change bridge initialization code to omit iptables modification
    * Linuxkit VMs do not have iptables

--- network/network.go
+++ network/network.go
@@ -4,9 +4,9 @@ import (
 	"fmt"
 	"github.com/vishvananda/netlink"
 	"github.com/vishvananda/netns"
-	"github.com/genuinetools/netns/bridge"
 	"net"
 	"runtime"
+	"strings"
 )
 
 type Network struct {
@@ -14,7 +14,7 @@ type Network struct {
 }
 
 func New() (*Network, error) {
-	br, err := bridge.Init(bridge.Opt{Name: "netns0", IPAddr: "10.0.0.1/16"})
+	br, err := initBridge()
 	if err != nil {
 		return nil, err
 	}
@@ -22,6 +22,56 @@ func New() (*Network, error) {
 	return &Network{
 		bridge: br,
 	}, nil
+
+	return &Network{}, nil
+}
+
+func initBridge() (*net.Interface, error) {
+	// bridge initialization code adapted from github.com/genuinetools/netns
+	// https://github.com/genuinetools/netns/blob/9d3c8173664af5f3d412ebdd7c66c013a965af4b/bridge/bridge.go#L34-L92
+	// iptables invocation removed for a bare linuxkit VM environment
+
+	var (
+		bridgeName = "netns0"
+		bridgeAddr = "10.0.0.1/16"
+		brigdgeMTU = 1500
+	)
+
+	bridge, err := net.InterfaceByName(bridgeName)
+	if err == nil {
+		// Bridge already exists, return early.
+		return bridge, nil
+	}
+
+	if !strings.Contains(err.Error(), "no such network interface") {
+		return nil, fmt.Errorf("getting interface %s failed: %v", bridgeName, err)
+	}
+
+	// Create *netlink.Bridge object.
+	la := netlink.NewLinkAttrs()
+	la.Name = bridgeName
+	la.MTU = brigdgeMTU
+	br := &netlink.Bridge{LinkAttrs: la}
+	if err := netlink.LinkAdd(br); err != nil {
+		return nil, fmt.Errorf("bridge creation for %s failed: %v", bridgeName, err)
+	}
+
+	// Setup ip address for bridge.
+	addr, err := netlink.ParseAddr(bridgeAddr)
+	if err != nil {
+		return nil, fmt.Errorf("parsing address %s failed: %v", bridgeAddr, err)
+	}
+
+	if err := netlink.AddrAdd(br, addr); err != nil {
+		return nil, fmt.Errorf("adding address %s to bridge %s failed: %v", addr.String(), bridgeName, err)
+	}
+
+	// Bring the bridge up.
+	if err := netlink.LinkSetUp(br); err != nil {
+		return nil, fmt.Errorf("bringing bridge %s up failed: %v", bridgeName, err)
+	}
+
+	return net.InterfaceByName(bridgeName)
 }
 
 func (n *Network) CreateVirtualEthernetPair(pid int) *netlink.Veth {
